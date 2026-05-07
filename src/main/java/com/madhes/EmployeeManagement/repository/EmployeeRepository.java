55d840322955941b4471179b30005f48306fa424
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-07 07:40:08 +0000

    service done

--- src/main/java/com/madhes/EmployeeManagement/repository/EmployeeRepository.java
+++ src/main/java/com/madhes/EmployeeManagement/repository/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.madhes.EmployeeManagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.madhes.EmployeeManagement.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findBySectorName(String sectorName);
 
 }
--- src/main/java/com/madhes/EmployeeManagement/service/EmployeeService.java
+++ src/main/java/com/madhes/EmployeeManagement/service/EmployeeService.java
@@ -1,9 +1,16 @@
 package com.madhes.EmployeeManagement.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.madhes.EmployeeManagement.dto.request.EmployeeRequestDTO;
 import com.madhes.EmployeeManagement.dto.response.EmployeeResponseDTO;
+import com.madhes.EmployeeManagement.entity.Employee;
+import com.madhes.EmployeeManagement.entity.Sector;
 import com.madhes.EmployeeManagement.repository.EmployeeRepository;
 import com.madhes.EmployeeManagement.repository.SectorRepository;
 
@@ -17,7 +24,46 @@ public class EmployeeService {
     private final SectorRepository sectorRepository;
 
     public EmployeeResponseDTO createEmployee(EmployeeRequestDTO requestDTO) {
-        
-        return null;
+
+        Sector sector = sectorRepository.findById(requestDTO.getSectorId())
+                                .orElseThrow(() -> new RuntimeException("Sector id not found"));
+
+        Employee employee = new Employee();
+        employee.setEmpId(requestDTO.getEmpId());
+        employee.setName(requestDTO.getName());
+        employee.setEmail(requestDTO.getEmail());
+        employee.setDomain(requestDTO.getDomain());
+        employee.setGrade(requestDTO.getGrade());
+        employee.setSector(sector);
+
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return mapToResponseDTO(savedEmployee);
+    }
+
+    public Page<EmployeeResponseDTO> getAllEmployees(Pageable pageable) {
+
+        Page<Employee> employee = employeeRepository.findAll(pageable);
+        return employee.map(this::mapToResponseDTO);
+    }
+
+    public List<EmployeeResponseDTO> getEmployeeBySector(String sectorName) {
+
+        List<Employee> employees =  employeeRepository.findBySectorName(sectorName);
+        return employees.stream().map(this::mapToResponseDTO).collect(Collectors.toList());
+    }
+
+    public EmployeeResponseDTO mapToResponseDTO(Employee employee) {
+        EmployeeResponseDTO dto = new EmployeeResponseDTO();
+
+        dto.setId(employee.getId());
+        dto.setEmpId(employee.getEmpId());
+        dto.setName(employee.getName());
+        dto.setEmail(employee.getEmail());
+        dto.setDomain(employee.getDomain());
+        dto.setGrade(employee.getGrade());
+        dto.setSectorName(employee.getSector().getName());
+
+        return dto;
     }
 }
