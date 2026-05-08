e48fb76fb4b63914a034e050f17081528c594be6
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-08 01:59:40 +0000

    final

--- src/main/java/com/madhes/EmployeeManagement/security/JwtAuthFilter.java
+++ src/main/java/com/madhes/EmployeeManagement/security/JwtAuthFilter.java
@@ -10,8 +10,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import org.springframework.security.core.userdetails.User;
-
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
 import org.springframework.stereotype.Component;
