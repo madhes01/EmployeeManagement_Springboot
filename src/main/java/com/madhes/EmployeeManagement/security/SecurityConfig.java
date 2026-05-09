b1ea4e327a4de097f939e4a19d0433adc29b4589
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-09 07:24:15 +0000

    Corrections_final

--- src/main/java/com/madhes/EmployeeManagement/security/SecurityConfig.java
+++ src/main/java/com/madhes/EmployeeManagement/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
